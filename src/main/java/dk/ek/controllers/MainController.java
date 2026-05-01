5f4b79e9ef8ac158999e5517963efb01926b6174
BertramFM <160260021+BertramFM@users.noreply.github.com>
2026-05-01 09:41:25 +0200

    MainController

--- src/main/java/dk/ek/controllers/MainController.java
+++ src/main/java/dk/ek/controllers/MainController.java
@@ -1,4 +1,16 @@
 package dk.ek.controllers;
 
+import dk.ek.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import org.jetbrains.annotations.NotNull;
+
 public class MainController {
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        app.get("/", ctx -> homePage(ctx, connectionPool));
+    }
+
+    private static void homePage(@NotNull Context ctx, ConnectionPool connectionPool) {
+        ctx.render("index.html");
+    }
 }
