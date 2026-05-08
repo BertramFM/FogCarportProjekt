a3d951e083ee0bb5e174834b75e6c8b56810684e
BertramFM <160260021+BertramFM@users.noreply.github.com>
2026-05-08 12:33:52 +0200

    route created

--- src/main/java/dk/ek/controllers/MainController.java
+++ src/main/java/dk/ek/controllers/MainController.java
@@ -8,9 +8,13 @@ import org.jetbrains.annotations.NotNull;
 public class MainController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.get("/", ctx -> homePage(ctx, connectionPool));
+        app.get("/flatRoof", ctx -> flatRoof(ctx, connectionPool));
     }
 
     private static void homePage(@NotNull Context ctx, ConnectionPool connectionPool) {
         ctx.render("index.html");
+    }
+    private static void flatRoof(@NotNull Context ctx, ConnectionPool connectionPool) {
+        ctx.render("flatRoof.html");
     }
 }
