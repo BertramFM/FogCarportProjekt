fb3f488427c21fb2f6453798f7cea1634a6e5560
BertramFM <160260021+BertramFM@users.noreply.github.com>
2026-05-08 14:32:10 +0200

    added path for new site

--- src/main/java/dk/ek/controllers/MainController.java
+++ src/main/java/dk/ek/controllers/MainController.java
@@ -9,12 +9,18 @@ public class MainController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.get("/", ctx -> homePage(ctx, connectionPool));
         app.get("/flatRoof", ctx -> flatRoof(ctx, connectionPool));
+        app.get("/highRoof", ctx -> highRoof(ctx, connectionPool));
     }
+
 
     private static void homePage(@NotNull Context ctx, ConnectionPool connectionPool) {
         ctx.render("index.html");
     }
     private static void flatRoof(@NotNull Context ctx, ConnectionPool connectionPool) {
         ctx.render("flatRoof.html");
+    }
+
+    private static void highRoof(@NotNull Context ctx, ConnectionPool connectionPool) {
+        ctx.render("highRoof.html");
     }
 }
