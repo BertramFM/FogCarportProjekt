be81e4c2a8a4676c6e0b0f21c63c1abd34f4a4ed
BertramFM <160260021+BertramFM@users.noreply.github.com>
2026-05-20 18:32:19 +0200

    Made things static, for easier explanations. commented out Register() because it's gonna get reworked and i need to push changes

--- src/main/java/dk/ek/controllers/SellerController.java
+++ src/main/java/dk/ek/controllers/SellerController.java
@@ -1,18 +1,21 @@
 package dk.ek.controllers;
 
 import dk.ek.entities.Employee;
+import dk.ek.exceptions.DatabaseException;
+import dk.ek.persistence.ConnectionPool;
 import dk.ek.persistence.CustomerMapper;
 import dk.ek.persistence.EmployeeMapper;
+import dk.ek.persistence.OrderMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
 public class SellerController {
 
-    public static void addRoutes(Javalin app, EmployeeMapper employeeMapper, CustomerMapper customerMapper) {
-        app.get("/seller", ctx -> showSellerPage(ctx, employeeMapper, customerMapper));
+    public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
+        app.get("/seller", ctx -> showSellerPage(ctx, connectionPool));
     }
 
-    private static void showSellerPage(Context ctx, EmployeeMapper employeeMapper, CustomerMapper customerMapper) {
+    private static void showSellerPage(Context ctx, ConnectionPool connectionPool) throws DatabaseException {
         Employee currentUser = ctx.sessionAttribute("currentUser");
 
         if (currentUser == null) {
@@ -25,8 +28,8 @@ public class SellerController {
             return;
         }
 
-        ctx.attribute("customers", customerMapper.getAllCustomers());
-        //ctx.attribute("orders", orderMapper.getAllOrders());
+        ctx.attribute("customers", CustomerMapper.getAllCustomers(connectionPool));
+        ctx.attribute("orders", OrderMapper.getAllOrders(connectionPool));
 
         ctx.render("seller.html");
     }
