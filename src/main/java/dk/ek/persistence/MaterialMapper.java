c36a43cdc0c30f48f22c7debab651572f33806a6
jasminagar <jasminagar21@gmail.com>
2026-05-22 10:12:35 +0200

    removed unnecessary import

--- src/main/java/dk/ek/persistence/MaterialMapper.java
+++ src/main/java/dk/ek/persistence/MaterialMapper.java
@@ -3,7 +3,6 @@ package dk.ek.persistence;
 import dk.ek.entities.Materials;
 import dk.ek.exceptions.DatabaseException;
 
-import javax.xml.crypto.Data;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
