943aa5f4655a32a388986cae8ae996ae31dafe75
jasminagar <jasminagar21@gmail.com>
2026-05-22 10:12:04 +0200

    made mapper methods static

--- src/main/java/dk/ek/persistence/CarportMapper.java
+++ src/main/java/dk/ek/persistence/CarportMapper.java
@@ -12,13 +12,8 @@ import java.util.ArrayList;
 import java.util.List;
 
     public class CarportMapper {
-        private ConnectionPool connectionPool;
 
-    public CarportMapper(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    public Carport getCarportById(int id) {
+    public static Carport getCarportById(int id, ConnectionPool connectionPool) {
         String sql = "SELECT * FROM orders WHERE id = ?";
         try(Connection connection = connectionPool.getConnection()){
             PreparedStatement ps = connection.prepareStatement(sql);
@@ -30,12 +25,7 @@ import java.util.List;
                 carport.setId(rs.getInt("id"));
                 carport.setWidth(rs.getInt("width"));
                 carport.setLength(rs.getInt("length"));
-                carport.setToolShed(rs.getBoolean("tool_shed"));
-                carport.setShedWidth(rs.getInt("shed_width"));
-                carport.setShedLength(rs.getInt("shed_length"));
-                carport.setNote(rs.getString("note"));
-                carport.setStatus(rs.getString("status"));
-                carport.setCreatedAt(rs.getDate("created_at"));
+
                 return carport;
             }
 
@@ -45,37 +35,37 @@ import java.util.List;
         return null;
     }
 
-    public List<Carport> getAllOrders() {
-        List<Carport> allOrders = new ArrayList<>();
-        String sql = "SELECT * FROM orders";
-        try(Connection connection = connectionPool.getConnection()){
-            PreparedStatement ps = connection.prepareStatement(sql);
-             ResultSet rs = ps.executeQuery();
+//    public List<Carport> getAllOrders(ConnectionPool connectionPool) {
+//        List<Carport> allOrders = new ArrayList<>();
+//        String sql = "SELECT * FROM orders";
+//        try(Connection connection = connectionPool.getConnection()){
+//            PreparedStatement ps = connection.prepareStatement(sql);
+//             ResultSet rs = ps.executeQuery();
+//
+//             while(rs.next()){
+//                 allOrders.add(new Carport(
+//                         rs.getInt("id"),
+//                         rs.getInt("customer_id"),
+//                         rs.getInt("employee_id"),
+//                         rs.getString("roof_type"),
+//                         rs.getInt("width"),
+//                         rs.getInt("length"),
+//                         rs.getBoolean("tool_shed"),
+//                         rs.getInt("shed_width"),
+//                         rs.getInt("shed_length"),
+//                         rs.getString("note"),
+//                         rs.getString("status"),
+//                         rs.getDate("created_at")
+//                 ));
+//             }
+//
+//        }catch (Exception e){
+//            e.printStackTrace();
+//        }
+//        return allOrders;
+//    }
 
-             while(rs.next()){
-                 allOrders.add(new Carport(
-                         rs.getInt("id"),
-                         rs.getInt("customer_id"),
-                         rs.getInt("employee_id"),
-                         rs.getString("roof_type"),
-                         rs.getInt("width"),
-                         rs.getInt("length"),
-                         rs.getBoolean("tool_shed"),
-                         rs.getInt("shed_width"),
-                         rs.getInt("shed_length"),
-                         rs.getString("note"),
-                         rs.getString("status"),
-                         rs.getDate("created_at")
-                 ));
-             }
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return allOrders;
-    }
-
-    public void saveCarport(Carport carport) {
+    public void saveCarport(Carport carport, ConnectionPool connectionPool) {
 
         String orderSql = """
                 INSERT INTO orders
