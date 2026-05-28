139b8e8711689d75fa3935215650e6e4774beea7
AlteredProject <125555532+AlteredProject@users.noreply.github.com>
2026-05-28 09:59:16 +0200

    changed so the order confirmation mail gets variables from the saved order instead of reusing form parameters

--- src/main/java/dk/ek/controllers/MailController.java
+++ src/main/java/dk/ek/controllers/MailController.java
@@ -2,8 +2,8 @@ package dk.ek.controllers;
 
 
 import dk.ek.config.ThymeleafConfig;
+import dk.ek.entities.Order;
 import dk.ek.services.MailService;
-import io.javalin.Javalin;
 import org.thymeleaf.context.Context;
 
 
@@ -12,21 +12,21 @@ public class MailController {
     public MailController() {
     }
 
-    public static void sendOrderConfirmation(io.javalin.http.Context ctx) {
+    public static void sendOrderConfirmation(io.javalin.http.Context ctx, Order order) {
 
         Context thymeleafContext = new Context();
 
-        thymeleafContext.setVariable("roofMaterialId", ctx.formParam("roofMaterialId"));
-        thymeleafContext.setVariable("roofAngle", ctx.formParam("roofAngle"));
-        thymeleafContext.setVariable("carportWidth", ctx.formParam("carportWidth"));
-        thymeleafContext.setVariable("carportLength", ctx.formParam("carportLength"));
-        thymeleafContext.setVariable("carportMaterialId", ctx.formParam("carportMaterialId"));
+        thymeleafContext.setVariable("roofMaterial", order.getRoofMaterial());
+        thymeleafContext.setVariable("roofAngle", order.getRoofAngle());
+        thymeleafContext.setVariable("carportWidth", order.getCarportWidth());
+        thymeleafContext.setVariable("carportLength", order.getCarportLength());
+        //thymeleafContext.setVariable("carportMaterialId", ctx.formParam("carportMaterialId"));
 
-        thymeleafContext.setVariable("toolShed", ctx.formParam("toolShed"));
-        thymeleafContext.setVariable("shedWidth", ctx.formParam("shedWidth"));
-        thymeleafContext.setVariable("shedLength", ctx.formParam("shedLength"));
+        thymeleafContext.setVariable("toolShed", order.getHasToolShed());
+        thymeleafContext.setVariable("shedWidth", order.getShedWidth());
+        thymeleafContext.setVariable("shedLength", order.getShedLength());
 
-        thymeleafContext.setVariable("note", ctx.formParam("note"));
+        thymeleafContext.setVariable("note", order.getNote());
 
         thymeleafContext.setVariable("firstname", ctx.formParam("firstname"));
         thymeleafContext.setVariable("lastname", ctx.formParam("lastname"));
